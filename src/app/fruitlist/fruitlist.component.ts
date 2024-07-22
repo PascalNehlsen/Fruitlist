6e398154a4966be56f7db938c95372e755d2c03c
Pascal Nehlsen <pascaldev92@gmail.com>
2024-07-22 11:45:53 +0200

    @input @nGClass @ngStyle

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SinglefruitComponent } from './singlefruit/singlefruit.component';
 
 @Component({
   selector: 'app-fruitlist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SinglefruitComponent],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss',
 })
 export class FruitlistComponent {
   howItWorks = 'fine';
+  fontColorGood = 'green';
+  fontColorBad = 'red';
 
   fruitlist = [
     {
